d6b9b350cc85f93ea77aac5f467eebe6bad57ff9
Maximiliano Duran <maximiliano.duran114@gmail.com>
2024-08-03 00:29:27 -0400

    number

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ function Contact() {
             d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
           />
         </svg>
-        <p className="font-serif">Telefono: +5693434231</p>
+        <p className="font-serif">Telefono: +56989014723</p>
       </div>
       <div className="flex justify-center flex-wrap items-center ">
         <svg
